f0959f099bc869facd28dba59834bd41094b145a
JYeonDev <whddus4837@gmail.com>
2021-10-20 21:14:41 +0900

    wishlist-app

--- src/components/wishlist.js
+++ src/components/wishlist.js
@@ -1,7 +1,40 @@
+import { useState } from "react";
+import { Modal, Button } from "react-bootstrap";
+import { useHistory } from "react-router";
+
 function Wishlist() {
+  /* let history = useHistory(); */
+  let categoryList = [];
+  let [categoryOnOff, setCategoryOnOff] = useState(false);
+
+  function onOff() {
+    setCategoryOnOff(!categoryOnOff);
+  }
+
   return (
     <>
-      <nav className="category">영역</nav>
+      <nav className="category">
+        <div style={{ margin: "20px" }}>카테고리</div>
+        <button className="category-plus" onClick={onOff}>
+          +
+        </button>
+        {categoryOnOff === true ? (
+          <Modal.Dialog>
+            <Modal.Header closeButton>
+              <Modal.Title>Modal title</Modal.Title>
+            </Modal.Header>
+
+            <Modal.Body>
+              <p>Modal body text goes here.</p>
+            </Modal.Body>
+
+            <Modal.Footer>
+              <Button variant="secondary">Close</Button>
+              <Button variant="primary">Save changes</Button>
+            </Modal.Footer>
+          </Modal.Dialog>
+        ) : null}
+      </nav>
       <div className="wishlist">구분</div>
     </>
   );
