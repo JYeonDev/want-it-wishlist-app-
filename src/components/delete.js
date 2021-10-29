f7fba1c6fd52ffb64c4be8d87a0cf7f34118f79e
JYeonDev <whddus4837@gmail.com>
2021-10-29 22:45:22 +0900

    wishlist app

--- src/components/delete.js
+++ src/components/delete.js
@@ -2,11 +2,15 @@ import { useState } from "react";
 
 function Delete() {
   let [data, setData] = useState(["임시데이터"]);
+  let [repair, setRepair] = useState(false);
+
+  function rep() {
+    setRepair(true);
+  }
 
   return (
     <>
       <div className="wishlist category" style={{ marginTop: "30px" }}>
-        <button className="category-plus">+</button>
         <div className="card-container">
           <div className="card-img">
             <img></img>
@@ -15,8 +19,11 @@ function Delete() {
             <p>제품명 : {data[0]}</p>
             <p>가격 : {data[0]}</p>
             <div className="card-href">
-              <button className="blue-button">복구</button>
+              <button className="blue-button" onClick={rep}>
+                복구
+              </button>
               <button className="red-button">영구삭제</button>
+              {repair === true ? <div>리얼 삭제 ?</div> : null}
             </div>
           </div>
         </div>
